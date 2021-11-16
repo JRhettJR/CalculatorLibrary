3b939ea844df6f9367427a216270f9c44fefd219
JRhettJR <62896206+JRhettJR@users.noreply.github.com>
2021-11-16 00:40:10 -0500

    Correcting mistakes

--- calculator.py
+++ calculator.py
@@ -1,57 +1,23 @@
-""" 
-Calculator library containing basic math operations.
-"""
+""" Calculator library containing basic math operations."""
 
 
 def add(first_term: int,
         second_term: int) -> int:
     """
-    add Description here
-
-    Parameters
-    ----------
+    add adds two int into one int
     first_term (int) : Parameter Description
-    second_term (int) : Parameter Description
-
-    Returns
-    -------
+    second_term (int) : Parameter Description 
     int : Return Item Description
-
-    Raises
-    ------
-    ValueError
-        If the value of first_term is None
-
     """
-    if first_term is None:
-        raise ValueError("first_term cannot be None")
-
-    print(first_term)
     return first_term + second_term
 
 
 def subtract(first_term: int,
              second_term: int) -> int:
     """
-    subtract Description here
-
-    Parameters
-    ----------
+    subtract two ints into one
     first_term (int) : Parameter Description
     second_term (int) : Parameter Description
-
-    Returns
-    -------
     int : Return Item Description
-
-    Raises
-    ------
-    ValueError
-        If the value of first_term is None
-
     """
-    if first_term is None:
-        raise ValueError("first_term cannot be None")
-
-    print(first_term)
-    return first_term + second_term
+    return first_term - second_term

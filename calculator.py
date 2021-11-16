976a442534474867b6143d0582efab0160ecaedb
JRhettJR <62896206+JRhettJR@users.noreply.github.com>
2021-11-16 01:35:03 -0500

    Adding all files

--- calculator.py
+++ calculator.py
@@ -1,23 +1,6 @@
-""" Calculator library containing basic math operations."""
-
-
-def add(first_term: int,
-        second_term: int) -> int:
-    """
-    add adds two int into one int
-    first_term (int) : Parameter Description
-    second_term (int) : Parameter Description 
-    int : Return Item Description
-    """
+def add(first_term: int, second_term: int) -> int:
     return first_term + second_term
 
 
-def subtract(first_term: int,
-             second_term: int) -> int:
-    """
-    subtract two ints into one
-    first_term (int) : Parameter Description
-    second_term (int) : Parameter Description
-    int : Return Item Description
-    """
+def subtract(first_term: int, second_term: int) -> int:
     return first_term - second_term
